2a13be56870b1ce070bc14f75fe176bc58f45696
8lackR0z3 <Darth9fater@protonmail.com>
2025-12-28 16:49:20 -0800

    Add capacitor.config.ts

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.blackrose.pokemonbattle',
+  appName: 'Pokemon Battle',
+  webDir: 'build'
+};
+
+export default config;
