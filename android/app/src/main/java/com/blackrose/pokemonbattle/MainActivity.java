4af13a914e6f398213a8a30d6524a79a6b99f568
8lackR0z3 <Darth9fater@protonmail.com>
2025-12-28 17:01:27 -0800

    Update android/app/src/main/java/com/blackrose/pokemonbattle/MainActivity.java

--- android/app/src/main/java/com/blackrose/pokemonbattle/MainActivity.java
+++ android/app/src/main/java/com/blackrose/pokemonbattle/MainActivity.java
@@ -1,5 +1,54 @@
 package com.blackrose.pokemonbattle;
 
-import com.getcapacitor.BridgeActivity;
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.view.Window;
+import android.view.WindowManager;
+import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends AppCompatActivity {
+    private WebView webView;
+
+    @SuppressLint("SetJavaScriptEnabled")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        // Fullscreen
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        );
+        
+        // Create WebView
+        webView = new WebView(this);
+        setContentView(webView);
+        
+        // Configure WebView
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
+        settings.setDomStorageEnabled(true);
+        settings.setAllowFileAccess(true);
+        settings.setAllowContentAccess(true);
+        settings.setUseWideViewPort(true);
+        settings.setLoadWithOverviewMode(true);
+        
+        webView.setWebViewClient(new WebViewClient());
+        
+        // Load the app
+        webView.loadUrl("file:///android_asset/public/index.html");
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
